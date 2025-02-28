2f9dcf515536cae69ed3a991f64dead3c7afd09f
Gabriel Barbosa <gbatistadev@gmail.com>
2025-02-28 14:24:36 -0300

    feat: create the initial structure of the cli with init function

---
+++ index.ts
@@ -0,0 +1,65 @@
+import chalk from "chalk";
+import { appendFile, existsSync, mkdir } from "fs";
+import { promisify } from "util";
+
+type status = "done" | "doing" | "to do";
+
+interface Task {
+  title: string;
+  status: status;
+}
+
+interface User {
+  username: string;
+  tasks: Task[];
+}
+
+// const read = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+const args = process.argv.slice(2);
+
+const mkdirAsync = promisify(mkdir);
+const appendFileAsync = promisify(appendFile);
+
+const init = async (): Promise<void> => {
+  const currDir = process.cwd();
+  const userDefault: User = {
+    username: "Gabriel",
+    tasks: [],
+  };
+
+  const configDir = `${currDir}/config`;
+  const tasksFilePath = `${configDir}/tasks.json`;
+
+  try {
+    if (!existsSync(configDir)) {
+      await mkdirAsync(configDir);
+    }
+    if (!existsSync(tasksFilePath)) {
+      const stringJson = JSON.stringify(userDefault, null, 2);
+      await appendFileAsync(tasksFilePath, stringJson);
+      console.log(chalk.green("Developer's task manager initialized successfully!"));
+    } else {
+      console.log(chalk.yellow("Developer's task manager was already initialized!"));
+    }
+  } catch (error) {
+    console.error(chalk.red(error));
+  }
+};
+
+function main() {
+  const mainCmd = args[0];
+
+  switch (mainCmd) {
+    case "init":
+      init();
+      break;
+
+    default:
+      break;
+  }
+}
+
+main();
